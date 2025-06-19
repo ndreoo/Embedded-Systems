43ac2e4e016bdf9d31a09ba12d9a1762feec1e92
ndreoo <andreascogna13@gmail.com>
2025-06-19 15:24:27 +0200

    add extensions

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 
 code --install-extension ms-vscode.cpptools-extension-pack --force
 code --install-extension hm.riscv-venus --force
+code --install-extension zhwu95.riscv --force
+code --install-extension ms-vscode.hexeditor --force
